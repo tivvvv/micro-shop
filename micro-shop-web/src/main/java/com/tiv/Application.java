71aad6d24d8f46e365d8720cd179c181d998adfd
tivvvv <18612553935@163.com>
2025-04-13 22:24:47 +0800

    feat: 初始化配置.

---
+++ micro-shop-web/src/main/java/com/tiv/Application.java
@@ -0,0 +1,16 @@
+package com.tiv;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@Configurable
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
